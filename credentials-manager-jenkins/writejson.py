6051c3afdc9b40f85fb3f1eceb417387ff9ec95a
nrohatgi <nishant.rohatgi28@gmail.com>
2023-06-16 10:30:18 +0530

    commit

--- credentials-manager-jenkins/writejson.py
+++ credentials-manager-jenkins/writejson.py
@@ -7,10 +7,6 @@ ssm = boto3.client('ssm')
 jenkins1_token = ssm.get_parameter(Name='/jenkins1', WithDecryption=True)
 jenkins2_token = ssm.get_parameter(Name='/jenkins2', WithDecryption=True)
 
-# jenkins_url = 'http://44.212.71.109:8080/manage/credentials/store/system/domain/_/'
-# jenkins_auth_username = 'admin'
-# jenkins_auth_token = '1187c4bb500c1cf2595c14fde4f06e0668'
-
 data = {
     "jenkins1": {
         "jenkins__url": "http://44.212.71.109:8080/manage/credentials/store/system/domain/_/",
@@ -34,20 +30,3 @@ data = {
         "crumbIssuer": "http://54.82.246.227:8080//crumbIssuer/api/json"
     }
 }
-
-# Writing the dictionary to a JSON file
-with open("cred.json", "w") as json_file:
-    json.dump(data, json_file, indent=4)
-
-# with open("cred.json", "r") as json_file:
-#     data = json.load(json_file)
-#
-# # Add a new dictionary to the data
-# new_jenkins = {
-#     "jenkins4": {
-#         "jenkins_url": "http://example.com/jenkins4",
-#         "auth_username": "admin4",
-#         "auth_password": "password4"
-#     }
-# }
-# data.update(new_jenkins)
